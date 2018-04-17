adf1df45c2d9e0885662d4976f2e23ba2de35e4a
Edgar7777 <losthedgar@hotmail.com>
2018-04-17 15:08:04 -0500

    primer codigo, tipos de variables y devolución de valores true o false
    variables

---
+++ 01-tipos-variables/tipos.js
@@ -0,0 +1,29 @@
+var nombre = "28";
+var edad = 28
+var fecha = new Date();
+var casado = false; // true
+console.log(casado ? "Verdadero" : "Falso");
+var casa = undefined;
+console.log(casa ? "Verdadero" : "Falso");
+var arreglo = []
+console.log(arreglo ? "Verdadero" : "Falso");
+//if else rapido
+var carro = null;
+console.log(carro ? "Verdadero" : "Falso");
+
+console.log(1 ? "Verdadero" : "Falso");
+console.log(0 ? "Verdadero" : "Falso");
+console.log(-1 ? "Verdadero" : "Falso");
+
+
+console.log(fecha)
+//con ctrl + alt + l se puede identar el codigo seleccionado
+
+if (nombre === edad) {
+
+    console.log("Verdadero")
+
+} else {
+    console.log("falso")
+}
+
